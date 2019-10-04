26b6a007e8bdde4ea55f5c0da0f9fbd8d4d3d7f5
yoelmz77 <>
2019-10-04 07:09:05 -0400

    Revert "Revert "Initial commit""
    This reverts commit 0111a3baec52f962eabc75014da77240b823a151.

---
+++ LAAR/LAAR/Models/License.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace LAAR.Models
+{
+    [Table("tblLicense")]
+    public class License
+    {
+        [Key]
+        public int LicenseID { get; set; }
+        public string Name { get; set; }
+        public DateTime Expires { get; set; }
+        public Staff SumittedBy { get; set; }
+        public Staff AprrovedBy { get; set; }
+        public string Attachement { get; set; }
+
+    }
+}
