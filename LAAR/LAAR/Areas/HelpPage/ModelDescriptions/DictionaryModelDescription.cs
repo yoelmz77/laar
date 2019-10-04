0111a3baec52f962eabc75014da77240b823a151
yoelmz77 <>
2019-10-04 07:07:59 -0400

    Revert "Initial commit"
    This reverts commit 96358b709c31421f16b17f2df681713247c0668d.

--- LAAR/LAAR/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace LAAR.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- LAAR/LAAR/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace LAAR.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
