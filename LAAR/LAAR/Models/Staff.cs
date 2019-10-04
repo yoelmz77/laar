e3b7aaa235a31f2458366c1aa7bf0431c96ced2b
yoelmz77 <>
2019-10-04 07:27:12 -0400

    commenting fields

--- LAAR/LAAR/Models/Staff.cs
+++ LAAR/LAAR/Models/Staff.cs
@@ -14,8 +14,8 @@ namespace LAAR.Models
     {
         [Key]
         public int StaffID { get; set; }
-        public string Name { get; set; }
-        public string Address { get; set; }
+        public string Name { get; set; } //Fullname, it could be breaddown into firstname, lastname
+        public string Address { get; set; } //Break Down address allows search by street address/city/state/zip
         public string Address2 { get; set; }
         public string City { get; set; }
         public string State { get; set; }
@@ -23,17 +23,15 @@ namespace LAAR.Models
         public string PhoneNumber { get; set; }
         public string PhoneNumber2 { get; set; }
         public string Email { get; set; }
-        public Position Position { get; set; }
-
-        public EducationalLevel EducationalLevel { get; set; }
-        public string LegalStatus { get; set; }
+        public Position Position { get; set; } //Administrator/OfficeManager/Staff/H2014/H2012/H2019/T1017
+        public EducationalLevel EducationalLevel { get; set; } //High School/Bachelor/Master
+        public string LegalStatus { get; set; } //Citizen/ Authorized to Work
         public int ProviderID { get; set; }
-
-        public List<License> Licenses {get; set;}
-        public string Rate { get; set; }
+        public List<License> Licenses {get; set;} 
+        public string Rate { get; set; } //Number?
         public string Credential { get; set; } //LMHC/LSW/BCBA/BCaBA/RBT/BS/Other(blank space) ?????
-        public string Initial { get; set; }
-        public string Signature { get; set; }
+        public string Initial { get; set; } //image format storage
+        public string Signature { get; set; } //image format storage
 
 
     }
